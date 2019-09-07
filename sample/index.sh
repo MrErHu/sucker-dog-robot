8fa4123836e27cf59c068b54f638ed0977dd7fef
lei.wang <wangelei_cs@163.com>
2019-09-07 23:43:10 +0800

    config

--- sample/index.sh
+++ sample/index.sh
@@ -2,7 +2,7 @@
 
 if [ -z $1 ]
 then
-  url='https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=03770be8-e341-4938-9c2a-0ccdd4dddd4e'
+  url='https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=92991272-d3b7-4e2d-b4fa-b257450c7e9a'
 else
   url=$1
 fi
