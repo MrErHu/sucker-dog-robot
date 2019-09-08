bc28e05ac9435808fb9621c335f4ec53ff475da2
lei.wang <wangelei_cs@163.com>
2019-09-08 16:25:47 +0800

    weather

--- src/config/weather.config.js
+++ src/config/weather.config.js
@@ -1,1 +1,1 @@
-export const URL = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=f4884548-6409-4361-b324-67e9781a6186';
+export const URL = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=8c9c1e53-95ce-413f-ab42-9aef7f23c786';
